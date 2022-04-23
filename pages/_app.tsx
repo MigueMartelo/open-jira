6f7f4d01bc0253ae661d02db5dc03fbefb3497ca
Migue Martelo <miguemartelo1031@gmail.com>
2022-04-22 21:35:43 -0500

    chore: add material-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { dark } from '@mui/material/styles/createPalette';
+
+const basicTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
