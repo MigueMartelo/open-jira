afc4530ee6ab21ae63c4e1c33ef15a7e509ee2b3
Migue Martelo <miguemartelo1031@gmail.com>
2022-04-23 09:25:07 -0500

    feature: create the UIProvider

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode, useReducer } from 'react';
+import { UIContext, UIReducer } from './';
+
+export interface UIState {
+  sidemenuOpen: boolean;
+}
+
+interface UIProviderProps {
+  children?: ReactNode;
+}
+
+const UI_INITIAL_STATE: UIState = {
+  sidemenuOpen: false,
+};
+
+export const UIProvider: FC<UIProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(UIReducer, UI_INITIAL_STATE);
+
+  return (
+    <UIContext.Provider value={UI_INITIAL_STATE}>{children}</UIContext.Provider>
+  );
+};
