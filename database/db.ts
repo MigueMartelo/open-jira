54a3e814afd9b34a29c49d03b40e96a744158a15
Migue Martelo <miguemartelo1031@gmail.com>
2022-07-09 17:31:37 -0500

    feat: config API and mongo connection

---
+++ database/db.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+/*
+ * @description: Database connection
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongoConnection = {
+  isConnected: 0,
+};
+
+export const connect = async () => {
+  if (mongoConnection.isConnected === 1) {
+    console.log('Ya estamos conectados');
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongoConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongoConnection.isConnected === 1) {
+      console.log('Usando conexión existente');
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect('...');
+  mongoConnection.isConnected = 1;
+  console.log('Conectado a MongoDB ...');
+};
+
+export const disconnect = async () => {
+  if (mongoConnection.isConnected !== 0) return;
+
+  await mongoose.disconnect();
+  console.log('Desconectado de MongoDB');
+};
