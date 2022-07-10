9081214d7ec0f08a0a66cfc465e8814120e6f8cc
Migue Martelo <miguemartelo1031@gmail.com>
2022-07-09 20:17:26 -0500

    feat: add endpoint to update an entry

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import mongoose from 'mongoose';
+import { db } from '../../../database';
+import { Entry, IEntry } from '../../../models';
+
+type Data = { message: string } | IEntry;
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: 'El id no es válido' });
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Método no existe!' });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query;
+  await db.connect();
+
+  const entryToUpdate = await Entry.findById(id);
+
+  if (!entryToUpdate) {
+    await db.disconnect();
+    return res.status(404).json({ message: 'No se encontró la entrada' });
+  }
+
+  const { description = entryToUpdate.description, status = entryToUpdate.status } = req.body;
+
+  try {
+    const updatedEntry = await Entry.findByIdAndUpdate(
+      id,
+      { description, status },
+      { runValidators: true, new: true }
+    );
+    await db.disconnect();
+    res.status(200).json(updatedEntry!);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    res.status(400).json({ message: 'Error al actualizar la entrada' });
+  }
+};
